1ff6865dee689a725f9f773480f1c74f21f59dc1
Rohit Nair <nair1111998@gmail.com>
2020-07-01 16:14:22 -0400

    Implement pulling in shop data from firestore and implement withspinner for loading

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
         });
       }
       else {
-        setCurrentUser(userAuth );
+        setCurrentUser(userAuth);
       }
     })
   }
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -3,17 +3,18 @@ import { createSelector } from 'reselect';
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
-);
-
-export const selectCollection = collectionUrlParam =>
-    createSelector(
-        [selectCollections],
-        collections => collections[collectionUrlParam]
+  [selectShop],
+  shop => shop.collections
 );
 
 export const selectCollectionsForPreview = createSelector(
-    [selectCollections],
-    collections => Object.keys(collections).map(key => collections[key])
+  [selectCollections],
+  collections =>
+    collections ? Object.keys(collections).map(key => collections[key]) : []
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => (collections ? collections[collectionUrlParam] : null)
+  );
